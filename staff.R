6c4a9c606cff771230530d74cb7d4dd86258b7bc
David Dobrowski <dobrowski@gmail.com>
2020-03-20 11:19:35 -0700

    joining files for Monterey

--- staff.R
+++ staff.R
@@ -17,3 +17,25 @@ staffdemo <- vroom(here("data","StaffDemo18.txt"))
 staffcred <- vroom(here("data","StaffCred18.txt"))
 staffschool <- vroom(here("data","StaffSchoolFTE18.txt"))
 
+
+
+#### Manipulated data -----
+
+staffschool <- staffschool %>% 
+    filter(str_detect(CountyName,"Monterey")) %>%
+    select(-FileCreated)
+
+staffdemo <- staffdemo %>% 
+    filter(str_detect(CountyName, "MONTEREY")) %>%
+    select(-c(FileCreated,DistrictCode:DistrictName ))
+
+joint <- staffschool %>%
+    left_join(staffdemo, by = c("AcademicYear", "RecID")) %>%
+    left_join(staffcred, by = c("AcademicYear", "RecID"))
+    
+
+
+
+
+
+#### End -----
