cac37c91323da2fecc5e563bec24e77f0bb07665
David Dobrowski <dobrowski@gmail.com>
2020-03-27 09:56:14 -0700

    id events

--- staff.R
+++ staff.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(here)
 library(vroom)
+library(readxl)
 
 
 #### Load files -------
@@ -16,7 +17,7 @@ library(vroom)
 staffdemo <- vroom(here("data","StaffDemo18.txt"))
 staffcred <- vroom(here("data","StaffCred18.txt"))
 staffschool <- vroom(here("data","StaffSchoolFTE18.txt"))
-
+oms <- read_excel(here("data","participant_export.xlsx"))
 
 
 #### Manipulated data -----
@@ -34,8 +35,31 @@ joint <- staffschool %>%
     left_join(staffcred, by = c("AcademicYear", "RecID"))
     
 
+oms_rev <- oms %>% 
+    select(-c(`Part Count`,
+           `End Date`:`Confirmation ID`,
+           `Phone 01`:`Address 02...29`,
+           `Address 01 (Home)`:`Address 02...34`,
+           Credit_01:`Modification Date`)
+           ) %>%
+    mutate(cds = str_replace(`CDS Code`,str_extract(`CDS Code`,"[:punct:]"),'')) %>%
+    mutate(cds = str_replace(cds,str_extract(cds,"[:punct:]"),'')) %>%
+    select(-`CDS Code`)
 
 
+people <- oms_rev %>%
+    group_by(`First Name`,`Last Name`) %>%
+    mutate(events = max(row_number())) %>%
+    select(`First Name`:School,cds,events) %>%
+    distinct()
+
+
+
+events <- oms_rev %>% 
+    select(`Event Title`, `Event ID`) %>%
+    distinct()
+
+write_csv(events, "events.csv")
 
 
 #### End -----
